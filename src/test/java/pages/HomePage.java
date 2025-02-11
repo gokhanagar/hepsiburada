88302769ed4ab73032461e2e55907892835a2b1c
gokhan agar <gokhanagar1@gmail.com>
2025-02-11 11:04:48 +0300

    V14

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -3,12 +3,17 @@ package pages;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static stepDefs.Hooks.driver;
 import static utility.BrowserUtils.verifyElementDisplayed;
 import static utility.BrowserUtils.waitForDOMStability;
+import static utility.BrowserUtils.waitForElementInteractable;
 import static utility.BrowserUtils.waitForPageToLoad;
 
 public class HomePage {
@@ -17,22 +22,25 @@ public class HomePage {
     private final By searchBar = By.cssSelector("input[type='search']");
     private final By cookieButton = By.cssSelector("button[id='onetrust-accept-btn-handler']");
 
-    // Belki duration dışarıdan parametre olarak verilebilir.
     public HomePage assertHomePage() {
         try {
-            waitForDOMStability(2);
-            /*
-            // Cookie butonu hala görünüyorsa tıkla
-            if (isElementDisplayed(cookieButton)) {
-                try {
-                    clickAndWaitForReload(cookieButton);
-                } catch (Exception e) {
-                    System.out.println("Cookie button click failed, continuing...");
-                }
-            }
-
-             */
-
+            // Sayfa yüklenene kadar bekle
+            waitForPageToLoad(20);
+            
+            // DOM stabilitesi için bekle
+            waitForDOMStability(5);
+            
+            // Explicit wait ile element görünür olana kadar bekle
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(selectedPopularProductText));
+            
+            // Element viewport'ta görünür olana kadar scroll
+            WebElement element = driver.findElement(selectedPopularProductText);
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+            
+            // Element interaktif olana kadar bekle
+            waitForElementInteractable(selectedPopularProductText);
+            
             verifyElementDisplayed(selectedPopularProductText);
             return this;
         } catch (Exception e) {
@@ -44,7 +52,6 @@ public class HomePage {
 
     public HomePage searchForProduct(String keyword) {
         try {
-
             waitForPageToLoad(10);
             waitForDOMStability(10);
 
