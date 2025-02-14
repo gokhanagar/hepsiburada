4f3a33d2da1bc16421eac642693133fc3e1377ff
gokhan agar <gokhanagar1@gmail.com>
2025-02-14 07:41:34 +0300

    V47

--- src/test/java/stepDefs/HomePageSteps/HomePageSteps.java
+++ src/test/java/stepDefs/HomePageSteps/HomePageSteps.java
@@ -26,7 +26,6 @@ public class HomePageSteps extends BasePage {
     @And("user accepts cookies")
     public void userAcceptsCookies() {
         homePage().acceptCookie();
-        homePage()
-                .checkAndHandleSecurityRedirect();
+        //homePage().checkAndHandleSecurityRedirect();
     }
 }
